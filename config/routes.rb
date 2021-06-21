fcc2db1abff0cc58e86d59221d9a0c24cd309e2b
Gayoon1234 <s3843584@student.rmit.edu.au>
2021-06-21 18:01:05 +1000

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+end
